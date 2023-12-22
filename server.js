3fefcadc74011ff3cf45f0059ba49a9069f8d8f9
Isaiah Branstetter <isaiah.bran@icloud.com>
2023-12-22 11:08:58 -0500

    TIP CALC ADDED

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
+// const express = require('express')
+// const app = express()
+// const PORT = process.env.PORT || 3000
+
+// app.get('/greeting/:name', (req, res) => {
+//     const name = req.params.name
+//     const greetings = [
+//         `Hello, ${Arthur}` 
+//     ]
+
+// })
+// app.listen(3000, () => {
+//     console.log('3000 is ACTIVE')
+// })
+
+
+
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
 
-app.get('/greeting/:name', (req, res) => {
-    const name = req.params.name
-    const greetings = [
-        `Hello, ${Arthur}` 
-    ]
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const total = parseFloat(req.params.total)
+    const tipPercentage = parseFloat(req.params.tipPercentage)
+
+    const tip = (total * tipPercentage) / 100
+    res.send( `Your tip will be: $${tip.toFixed(2)}`)
 
 })
+
 app.listen(3000, () => {
-    console.log('3000 is ACTIVE')
+    console.log('Tip 3000 is ACTIVE')
 })
+
+
+
